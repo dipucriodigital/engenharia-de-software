afc0b3135deb4b507b6b7390613928b6fdf562be
lcromao <lc.romao98@gmail.com>
2023-12-04 20:35:07 -0300

    Modificacoes feitas em aula

--- mvp-qualidade-seguranca-sistemas-inteligentes/exemplo_mvp/api/test_modelos.py
+++ mvp-qualidade-seguranca-sistemas-inteligentes/exemplo_mvp/api/test_modelos.py
@@ -2,6 +2,8 @@ from model.avaliador import Avaliador
 from model.carregador import Carregador
 from model.modelo import Model
 
+# To run: pytest -v test_modelos.py
+
 # Instanciação das Classes
 carregador = Carregador()
 modelo = Model()
@@ -19,10 +21,11 @@ X = dataset.iloc[:, 0:-1]
 Y = dataset.iloc[:, -1]
     
 # Método para testar o modelo de Regressão Logística a partir do arquivo correspondente
+# O nome do método a ser testado necessita começar com "test_"
 def test_modelo_lr():  
     # Importando o modelo de regressão logística
     lr_path = 'ml_model/diabetes_lr.pkl'
-    modelo_lr = Model.carrega_modelo(lr_path)
+    modelo_lr = modelo.carrega_modelo(lr_path)
 
     # Obtendo as métricas da Regressão Logística
     acuracia_lr, recall_lr, precisao_lr, f1_lr = avaliador.avaliar(modelo_lr, X, Y)
@@ -38,7 +41,7 @@ def test_modelo_lr():
 def test_modelo_knn():
     # Importando modelo de KNN
     knn_path = 'ml_model/diabetes_knn.pkl'
-    modelo_knn = Model.carrega_modelo(knn_path)
+    modelo_knn = modelo.carrega_modelo(knn_path)
 
     # Obtendo as métricas do KNN
     acuracia_knn, recall_knn, precisao_knn, f1_knn = avaliador.avaliar(modelo_knn, X, Y)
